8dbb12bdd543cedee71db89a3683d36742609f02
bayuuuaji <bayuuuajisantoso@gmail.com>
2021-12-31 14:00:41 +0700

    1 DOM selection

---
+++ 1 DOM selection/script.js
@@ -0,0 +1,32 @@
+// DOM Selection
+// // getElementById() -> menangkap element
+// const judul = document.getElementById('judul');
+// judul.style.color = 'red';
+// judul.style.backgroundColor = '#ccc';
+// judul.innerHTML = 'Bayu Aji';
+
+// // document.getElementsByTagName() -> HTMLCollections
+// const p = document.getElementsByTagName('p');
+// for (var i = 0; i < p.length; i++){
+//     p[i].style.backgroundColor = 'lightblue';
+// }
+
+// const h1 = document.getElementsByTagName('h1')[0];
+// h1.style.fontSize = '50px';
+
+// // document.getElementsByClassName -> HTMLCollections
+// const p1 = document.getElementsByClassName('p1')[0];
+// p1.innerHTML = 'ini diubah dari javascript';
+
+// // document.querySelector() -> element
+// const p4 = document.querySelector('#b p');
+// p4.style.backgroundColor = 'green';
+
+// const li2 = document.querySelector('section#b ul li:nth-child(2)');
+// li2.style.backgroundColor = 'orange';
+
+// // document.querySelectorAll
+// const p = document.querySelectorAll('p');
+// for (var i = 0; i < p.length; i++) {
+//   p[i].style.backgroundColor = 'aqua';
+// }
