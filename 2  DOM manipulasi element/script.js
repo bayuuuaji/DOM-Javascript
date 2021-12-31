654063812c2aff2bbae8933d991d952a0d9b7828
bayuuuaji <bayuuuajisantoso@gmail.com>
2021-12-31 17:44:55 +0700

    2  DOM manipulasi element

---
+++ 2  DOM manipulasi element/script.js
@@ -0,0 +1,33 @@
+// const judul = document.getElementById('judul');
+// judul.innerHTML = ('Ini Judul');
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML = 'hello world';
+
+// const judul = document.querySelector('#judul');
+// judul.style.color = 'lightblue';
+// judul.style.backgroundColor = 'salmon';
+
+// const a = document.querySelector('section#a a');
+// // set attribute
+// a.setAttribute('id', 'link');
+// // get attribute
+// a.getAttribute('href');
+// //  remove attribute
+// a.removeAttribute('href');
+
+// const p2 = document.querySelector('.p2');
+// // add class
+// p2.classList.add('label');
+// p2.classList.add('satu');
+// p2.classList.add('dua');
+// p2.classList.add('tiga');
+// // remove class
+// p2.classList.remove('label');
+// // toggle class
+// p2.classList.toggle('label');
+// // item class
+// p2.classList.item(2);
+// // contains class
+// p2.classList.contains('dua');
+// // replace class
+// p2.classList.replace('label', 'nol');
